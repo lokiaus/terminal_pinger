3cb33ba4e20281e9596350bd403400b0f06974f4
Loki <126240945+lokiaus@users.noreply.github.com>
2024-03-18 14:22:32 +1100

    improved syntax for clarity, improved data type handling

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import logging
 from ping3 import ping
 from colorama import just_fix_windows_console, Fore
 
-NO_OF_PINGS = 20  # Number of pings to send
-DESTINATION_HOST = "google.com"  # Destination host to ping
+NO_OF_PINGS: int = 20  # Number of pings to send
+DESTINATION_HOST: str = "google.com"  # Destination host to ping
 
 
-def get_trend(avg_ping, prev_avg=None):
+def get_trend(avg_ping, prev_avg=None) -> str:
     """
     Function to determine the trend based on the average ping time.
 
@@ -23,7 +23,7 @@ def get_trend(avg_ping, prev_avg=None):
     if prev_avg is None or avg_ping == prev_avg:
         return f"{Fore.RESET}  -"
 
-    change = avg_ping - prev_avg
+    change: float = avg_ping - prev_avg
 
     thresholds = [
         (-prev_avg * 0.3, f"{Fore.LIGHTCYAN_EX} ↑↑"),
@@ -43,7 +43,7 @@ def get_trend(avg_ping, prev_avg=None):
     return f"{Fore.RESET}  -"
 
 
-def signal_quality(avg_ping):
+def signal_quality(avg_ping) -> str:
     """
     Function to determine the quality of the signal based on the average ping time.
 
@@ -64,7 +64,7 @@ def signal_quality(avg_ping):
     return quality
 
 
-def print_status(success, **kwargs):
+def print_status(success, **kwargs) -> None:
     """
     Function to print the status message.
 
@@ -81,7 +81,7 @@ def print_status(success, **kwargs):
     if success:
         last_ping = kwargs['last_ping']
         progress_str = kwargs.get('progress_str')
-        print_str = f"\r{Fore.LIGHTMAGENTA_EX}current:{Fore.RESET} {int(last_ping):05}ms, "
+        print_str: str = f"\r{Fore.LIGHTMAGENTA_EX}current:{Fore.RESET} {last_ping:05.0f}ms, "
         if progress_str:
             print_str += f"{Fore.LIGHTMAGENTA_EX}progress:{Fore.RESET} {progress_str}"
         else:
@@ -89,7 +89,7 @@ def print_status(success, **kwargs):
             prev_avg = kwargs['prev_avg']
             loss = kwargs['loss']
             lc = Fore.LIGHTRED_EX if loss > 30 else Fore.LIGHTYELLOW_EX if loss > 0 else Fore.LIGHTGREEN_EX
-            print_str += (f"{Fore.LIGHTMAGENTA_EX}avg:{Fore.RESET} {avg_ping:05}ms, "
+            print_str += (f"{Fore.LIGHTMAGENTA_EX}avg:{Fore.RESET} {avg_ping:05.0f}ms, "
                           f"{Fore.LIGHTMAGENTA_EX}loss:{Fore.RESET} {lc}{loss:03}%{Fore.RESET}, "
                           f"{Fore.LIGHTMAGENTA_EX}quality:{Fore.RESET} "
                           f"{signal_quality(avg_ping if last_ping else None)}"
@@ -101,7 +101,7 @@ def print_status(success, **kwargs):
               f"{Fore.RESET}", end='', flush=True)
 
 
-def main():
+def main() -> None:
     """
     Main function to handle inputs and send and process pings.
 
@@ -110,24 +110,24 @@ def main():
     calculates the average ping time, and prints the status message.
 
     """
-    no_of_pings = NO_OF_PINGS
-    destination_host = DESTINATION_HOST
-    inp = input(f"Number of pings ({NO_OF_PINGS}): ")
+    no_of_pings: int = NO_OF_PINGS
+    destination_host: str = DESTINATION_HOST
+    inp: str = input(f"Number of pings ({NO_OF_PINGS}): ")
     if inp.isdigit() and int(inp) > 0:
         no_of_pings = int(inp)
         inp = input(f"Target host ({DESTINATION_HOST}): ")
         destination_host = inp if inp else DESTINATION_HOST
 
     logging.info(f"Sending {no_of_pings} pings to {destination_host}")
-    ping_list = []
-    losses = []
-    prev_avg = 0
+    ping_list: list[float] = []
+    losses: list[bool] = []
+    prev_avg: float = 0.0
 
     while True:
         try:
-            last_ping = ping(destination_host, unit="ms")
+            last_ping: float = ping(destination_host, unit="ms")
 
-            is_loss = last_ping == 0.0 or last_ping is None or False  # Check for loss
+            is_loss: bool = last_ping == 0.0 or last_ping is None or False  # Check for loss
             losses.append(is_loss)
             losses = losses[-no_of_pings:]  # Keep only the last NO_OF_PINGS losses
 
@@ -137,13 +137,13 @@ def main():
                 last_ping = round(last_ping, 1)
 
             if len(ping_list) >= no_of_pings:  # If there are enough pings, calculate average and print status
-                loss = round(int((sum(losses) / no_of_pings) * 100), 1)
-                avg_ping = round(sum(ping_list) / len(ping_list))
+                loss: int = round(int((sum(losses) / no_of_pings) * 100), 1)
+                avg_ping: float = sum(ping_list) / len(ping_list)
                 print_status(True, last_ping=last_ping if last_ping else 0,
                              avg_ping=avg_ping, prev_avg=prev_avg, loss=loss)
                 prev_avg = avg_ping
             else:
-                progress_str = f"{len(ping_list)} of {no_of_pings}"
+                progress_str: str = f"{len(ping_list)} of {no_of_pings}"
                 print_status(True, last_ping=last_ping, progress_str=progress_str)
         except Exception as e:
             logging.exception(f"An unexpected error occurred: {e}")
